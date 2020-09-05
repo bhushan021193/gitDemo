6a34d76df1f456458111b6747d3d68d7a33092b4
hp <hp@LAPTOP-87UI5V04>
2020-09-05 14:12:25 +0530

    vv

--- gitDemo/src/com/cjc/Demo.java
+++ gitDemo/src/com/cjc/Demo.java
@@ -1,6 +1,14 @@
 package com.cjc;
 
 public class Demo {
+	
+	
+	public void sum() {
+		
+		
+		System.out.println("sum method");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("main method");
 	}
